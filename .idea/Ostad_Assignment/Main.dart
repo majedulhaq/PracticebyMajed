cb572f769df51d103e32b3a0e519ec8aa3b42219
majedul_haq <majedulhaq550@gmail.com>
2024-06-27 10:14:28 +0600

    kk

--- .idea/Ostad_Assignment/Main.dart
+++ .idea/Ostad_Assignment/Main.dart
@@ -1,1 +1,13 @@
-wifi password jana ase ni ?
+Stream<int> learnstrm() async* {
+  for(int i=1; i<=10; i++){
+   await Future.delayed(Duration(seconds: 3));
+    yield i;
+  }
+}
+void main(){
+  learnstrm().listen(
+      (myData){
+        print(myData);
+      }
+  );
+}
